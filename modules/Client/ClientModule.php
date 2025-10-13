3e263e35b4e03771f83a5ac634767b18df07dc57
Chris <j.c.durf@gmail.com>
2025-10-12 18:28:53 -0600

    feat: Add ClientModule and Gateway classes; enhance UserModule with auth gates documentation

---
+++ modules/Client/ClientModule.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+namespace Modules\Client;
+
+use Proto\Module\Module;
+
+/**
+ * ClientModule
+ *
+ * Adds the client features to the platform.
+ *
+ * @package Modules\Client
+ */
+class ClientModule extends Module
+{
+	/**
+	 * This will activate the module.
+	 *
+	 * @return void
+	 */
+	public function activate(): void
+	{
+	}
+}
---
+++ modules/Client/Gateway/Gateway.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+namespace Modules\Client\Gateway;
+
+/**
+ * Gateway
+ *
+ * @package Modules\Client\Gateway
+ */
+class Gateway
+{
+}
