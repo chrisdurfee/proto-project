97af5793790a001247da80b6a9e5396cd27b2eef
Chris <j.c.durf@gmail.com>
2025-10-13 21:12:27 -0600

    feat: Refactor Modal to use context for client data; simplify button click handlers in PageHeader

--- apps/crm/src/modules/clients/components/pages/client/summary/summary-page.js
+++ apps/crm/src/modules/clients/components/pages/client/summary/summary-page.js
@@ -10,13 +10,12 @@ import { ConversationSection } from "./conversation/conversation-section.js";
 /**
  * This will create a permission modal.
  *
- * @param {object} item
  * @param {object} parent
  * @returns {object}
  */
-const Modal = (item, { context }) => (
+const Modal = ({ context }) => (
 	ClientModal({
-		item,
+		item: context.data.client,
 		onSubmit: (data) => context.data.client = data.get()
 	})
 );
@@ -34,10 +33,10 @@ const PageHeader = (client) => (
 		H2({ class: 'text-2xl font-medium' }, 'Client Summary'),
 		Div({ class: 'flex flex-row gap-x-2' }, [
 			Div({ class: 'hidden lg:inline-flex' }, [
-				Button({ variant: 'withIcon', class: 'text-muted-foreground outline', icon: Icons.pencil.square, click: (e, parent) => Modal(client, parent) }, 'Edit'),
+				Button({ variant: 'withIcon', class: 'text-muted-foreground outline', icon: Icons.pencil.square, click: (e, parent) => Modal(parent) }, 'Edit'),
 			]),
 			Div({ class: 'flex lg:hidden mr-4' }, [
-				Tooltip({ content: 'Edit', position: 'left' }, Button({ variant: 'icon', class: 'outline', icon: Icons.pencil.square, click: (e, parent) => Modal(client, parent) }))
+				Tooltip({ content: 'Edit', position: 'left' }, Button({ variant: 'icon', class: 'outline', icon: Icons.pencil.square, click: (e, parent) => Modal(parent) }))
 			])
 		])
 	])
