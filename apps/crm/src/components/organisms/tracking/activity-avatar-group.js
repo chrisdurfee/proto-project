59cb72e6298b9188e51cb58a5ac7943d1dbefce8
Chris <j.c.durf@gmail.com>
2025-10-16 18:47:41 -0600

    refactor: update Group component styling to include margin for better layout

--- apps/crm/src/components/organisms/tracking/activity-avatar-group.js
+++ apps/crm/src/components/organisms/tracking/activity-avatar-group.js
@@ -81,7 +81,7 @@ const UserContainer = (props) => Div({ class: 'user' }, [
  * @returns {object}
  */
 const Group = () => Div({
-	class: 'flex gap-x-2',
+	class: 'flex gap-x-2 mx-2',
 	for: ['rows', UserContainer]
 });
 
