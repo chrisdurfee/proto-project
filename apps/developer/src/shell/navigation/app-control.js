1a0e8dad93966f3940d2d6dacf30e712a1076658
tech_e <chris.durfee@dentalqore.com>
2025-10-27 17:15:26 -0600

    fix: improve PWA compatibility and touch responsiveness across applications

--- apps/developer/src/shell/navigation/app-control.js
+++ apps/developer/src/shell/navigation/app-control.js
@@ -5,6 +5,28 @@ import { getMobileOptions } from "./mobile-options.js";
 import { MobileNavigation } from "./mobile/mobile-navigation.js";
 
 /**
+ * This will detect if the app is running in standalone (PWA) mode.
+ *
+ * @returns {boolean}
+ */
+const isStandalone = () =>
+{
+	// @ts-ignore - standalone is iOS-specific
+	return window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone;
+};
+
+/**
+ * This will get the navigation height class based on display mode.
+ * - Browser mode: Fixed 56px (safe area causes floating)
+ * - PWA mode: 56px + safe area (gesture bar needs space)
+ *
+ * @returns {string}
+ */
+const getNavBottom = () => isStandalone()
+	? 'bottom-0 pb-[env(safe-area-inset-bottom)]'
+	: 'bottom-[env(safe-area-inset-bottom)] h-[56px]';
+
+/**
  * This will get the hover class.
  *
  * @returns {string}
@@ -19,41 +41,13 @@ const getHoverClass = () => Configs.useShortNav ? '' : 'lg:max-w-[330px] lg:hove
  * @returns {object}
  */
 const AppContainer = Atom((props, children) => ({
-    ...props,
-    class: `app-nav-container bg-background/80 backdrop-blur-md fixed sm:top-0 bottom-0 left-0 w-full lg:w-[64px]
-    sm:h-full z-10 lg:z-20 lg:overflow-y-auto overflow-x-hidden shadow-md border-t sm:border-r sm:border-t-0 lg:border-r
-    ${getHoverClass()}`,
-    children
+	...props,
+	class: `app-nav-container bg-background/80 backdrop-blur-md fixed sm:top-0 ${getNavBottom()} left-0 w-full lg:w-[64px]
+	sm:h-full z-10 lg:z-20 lg:overflow-y-auto overflow-x-hidden shadow-md border-t sm:border-r sm:border-t-0 lg:border-r
+	sm:pb-0
+	${getHoverClass()}`,
+	children
 }));
-
-/**
- * This will map the mobile options.
- *
- * @param {array} options
- * @param {array} mobileOptions
- * @param {function} callBack
- * @returns {void}
- */
-const mapMobileOptions = (options, mobileOptions, callBack) =>
-{
-    options.forEach(option =>
-    {
-        if (option.options)
-        {
-            mapMobileOptions(option.options, mobileOptions, callBack);
-            return;
-        }
-
-        /**
-         * We also want to add a callBack to ignore the hover to the main options.
-         */
-        option.callBack = callBack;
-        if (option.mobileOrder !== undefined)
-        {
-            mobileOptions.push(option);
-        }
-    });
-};
 
 /**
  * AppControl
@@ -65,122 +59,130 @@ const mapMobileOptions = (options, mobileOptions, callBack) =>
  */
 export class AppControl extends Component
 {
-    /**
-     * This will declare the properties of the component.
-     *
-     * @returns {void}
-     */
-    declareProps()
-    {
-        /**
-         * @member {number|null} timer
-         */
-        this.timer = null;
-    }
+	/**
+	 * This will declare the properties of the component.
+	 *
+	 * @returns {void}
+	 */
+	declareProps()
+	{
+		/**
+		 * @member {number|null} timer
+		 */
+		this.timer = null;
+	}
 
-    /**
-     * This will set the app controll state to be stored in the local storage.
-     *
-     * @returns {Data}
-     */
-    afterSetup()
-    {
-        /**
-         * This will set up the pinned data to allow it to
-         * be stored in the local storage.
-         */
-        const storageKey = 'pinned';
-        const data = this.state;
-        data.setKey(storageKey);
+	/**
+	 * This will set the app controll state to be stored in the local storage.
+	 *
+	 * @returns {Data}
+	 */
+	afterSetup()
+	{
+		/**
+		 * This will set up the pinned data to allow it to
+		 * be stored in the local storage.
+		 */
+		const storageKey = 'pinned';
 
-        /**
-         * This will resume the data from the local storage.
-         */
-        data.resume();
-        return data;
-    }
+		// @ts-ignore
+		const data = this.state;
+		data.setKey(storageKey);
 
-    /**
-     * This will render the component.
-     *
-     * @returns {object}
-     */
+		/**
+		 * This will resume the data from the local storage.
+		 */
+		data.resume();
+		return data;
+	}
+
+	/**
+	 * This will render the component.
+	 *
+	 * @returns {object}
+	 */
 	render()
 	{
-        const callBack = this.ignoreHover.bind(this);
-        // @ts-ignore
-        const mobileOptions = getMobileOptions(this.options, callBack);
+		const callBack = this.ignoreHover.bind(this);
+		// @ts-ignore
+		const mobileOptions = getMobileOptions(this.options, callBack);
 
 		return AppContainer(
-            {
-                onState: [
-                    ['ignoreHover', {
-                        ignoreHover: true
-                    }],
-                    ['pinned', {
-                        pinned: true
-                    }]
-                ],
-                mouseleave: this.removeIgnore.bind(this)
-            },
-            [
-                // @ts-ignore
-                new MainNavigation({ options: this.options }),
-                new MobileNavigation({ options: mobileOptions })
-            ]
-        );
-    }
+			{
+				onState: [
+					['ignoreHover', {
+						ignoreHover: true
+					}],
+					['pinned', {
+						pinned: true
+					}]
+				],
+				mouseleave: this.removeIgnore.bind(this)
+			},
+			[
+				/**
+				 * This will create a navigation for the main and mobile navigation.
+				 */
+				// @ts-ignore
+				new MainNavigation({ options: this.options }),
+				new MobileNavigation({ options: mobileOptions })
+			]
+		);
+	}
 
-    /**
-     * This will ignore the hover.
-     *
-     * @returns {void}
-     */
-    ignoreHover()
-    {
-        this.state.ignoreHover = true;
+	/**
+	 * This will ignore the hover.
+	 *
+	 * @returns {void}
+	 */
+	ignoreHover()
+	{
+		// @ts-ignore
+		this.state.ignoreHover = true;
 
-        const DELAY_MILLISECONDS = 100;
-        window.setTimeout(() => this.removeIgnore(), DELAY_MILLISECONDS);
-    }
+		const DELAY_MILLISECONDS = 100;
+		window.setTimeout(() => this.removeIgnore(), DELAY_MILLISECONDS);
+	}
 
-    /**
-     * This will remove the ignore.
-     *
-     * @returns {void}
-     */
-    removeIgnore()
-    {
-        window.clearTimeout(this.timer);
+	/**
+	 * This will remove the ignore.
+	 *
+	 * @returns {void}
+	 */
+	removeIgnore()
+	{
+		window.clearTimeout(this.timer);
 
-        const DELAY_MILLISECONDS = 400;
-        const callBack = () => this.state.ignoreHover = false;
-        this.timer = window.setTimeout(callBack, DELAY_MILLISECONDS);
-    }
+		const DELAY_MILLISECONDS = 400;
+		// @ts-ignore
+		const callBack = () => this.state.ignoreHover = false;
+		this.timer = window.setTimeout(callBack, DELAY_MILLISECONDS);
+	}
 
-    /**
-     * This will setup the states.
-     *
-     * @returns {object}
-     */
+	/**
+	 * This will setup the states.
+	 *
+	 * @returns {object}
+	 */
 	setupStates()
 	{
-        /**
-         * This will set a global state id for the app control.
-         */
+		/**
+		 * This will set a global state id for the app control.
+		 */
 		this.stateTargetId = 'app-control';
 
 		return {
-            ignoreHover: false,
+			ignoreHover: false,
 
-            /**
-             * This will add a callback to store the pinned state
-             * in the local storage when it changes.
-             */
-            pinned: {
-                state: false,
-                callBack: () => this.state.store()
-            }
+			/**
+			 * This will add a callback to store the pinned state
+			 * in the local storage when it changes.
+			 */
+			pinned: {
+				state: false,
+				// @ts-ignore
+				callBack: () => this.state.store()
+			}
 		};
 	}
 }
