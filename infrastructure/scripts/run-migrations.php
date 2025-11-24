0b4bd08a5a332cd1891075195e094d2c8430fcc3
Chris <j.c.durf@gmail.com>
2025-11-23 19:19:56 -0700

    Re-enable Error::disable() in migration script
    Proto error tracking breaks the Docker container build process.
    Keep native PHP error reporting enabled but disable Proto's
    error tracking system to prevent container build issues.

--- infrastructure/scripts/run-migrations.php
+++ infrastructure/scripts/run-migrations.php
@@ -12,7 +12,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// Error::disable();
+Error::disable();
 
 /**
  * Initialize the Proto framework
